be104ff5843ac82d430677b29e4daf00fbb05584
COR Students <corstudents@Matts-MBP-5.localdomain>
2018-10-22 12:11:36 -0400

    finished assessment

---
+++ assessment1.js
@@ -0,0 +1,62 @@
+//Question1: Create a variable of each of the follwing data types String,Int,Bool,Float
+//Question2:Create a variable called trueOrFalse. Create an if statement so that if the variable is true the number 1 is returned. Otherwise the number 2 is returned.
+//Question3:Create a for loop that counts every even number from 1-100 starting at 0
+//Question4: Create a variable that is a bool and is equal to true create a while loop that will print "hello" to the console 20 times and then chage that variable to false ending the loop.
+//Question5: Create a function that can determin if a parameter is true or false.  If it is true return the string "true". Otherwise return the string "false"
+//Bonus Question: What does D.R.Y. stand for?
+
+
+//Quesiton 1:
+
+// let person = "Maurice";
+// let age = 30;
+// let maleGender = true;
+// let pi = 3.142856;
+
+// Question2
+
+// let trueOrFalse = true
+// if (trueOrFalse===true){
+//     console.log("1");
+// }else {
+//     console.log("2");
+// }
+
+
+//Question3
+
+
+// for (i=0;i<101;i+=2){
+//     console.log(i);
+// }
+
+// Question4:  
+
+
+
+// let counter = 0;
+// while (greeting = true) {
+//     console.log("hello");
+//     counter++;
+//  if (counter === 20)
+//     greeting = false
+
+// }
+
+
+//Question5:
+
+// let trueOrFalse;
+
+// if (trueOrFalse) {
+//     return "true";
+// }
+
+// else  {
+//     return "false"
+    
+
+
+//Bonus:
+
+//Dont Replicate Yourself 
